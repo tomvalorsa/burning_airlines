8eaa323327307ca3a9c2265286d9f450cdde649d
Julia <julia.ryanx@gmail.com>
2015-03-13 15:59:56 +1100

    expand admin access

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def landing
-    
+  end
+  def search
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
-      redirect_to flights_path
+      redirect_to search_path
     else
       render :new
     end
