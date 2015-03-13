ecd7946245bbe02efd5dcccf38640ef5ad2cf45e
Julia <julia.ryanx@gmail.com>
2015-03-14 10:30:53 +1100

    update admin method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
   def landing
   end
+
   def search
   end
+
+  private
   def check_if_admin
     redirect_to(login_path) unless @current_user.present? && @current_user.is_admin?
   end
