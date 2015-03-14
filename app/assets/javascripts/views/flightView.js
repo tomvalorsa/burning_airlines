f37b7215f438f303128f03f438e563547c350f32
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-14 15:37:12 +1100

    flight view accessing flight info

--- app/assets/javascripts/views/flightView.js
+++ app/assets/javascripts/views/flightView.js
@@ -6,7 +6,13 @@ app.FlightView = Backbone.View.extend({
   render: function () {
     var flightViewTemplate = $('#flightView-template').html();
     var flightViewHTML = _.template(flightViewTemplate);
-
+    debugger;
     this.$el.html(flightViewHTML(this.model.toJSON()));
+
+    // // Save the actual flight object to a var
+    // var flightDetails = this.model.toJSON();
+
+    // // Airplane ID in a var
+    // var airplane = flightDetails.airplane_id;
   }
 });
