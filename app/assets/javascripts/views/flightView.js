26f0c80a21576a4e446a0bd05d4b5200e0d51336
Julia <julia.ryanx@gmail.com>
2015-03-15 02:30:39 +1100

    working on seats plan render

--- app/assets/javascripts/views/flightView.js
+++ app/assets/javascripts/views/flightView.js
@@ -6,8 +6,22 @@ app.FlightView = Backbone.View.extend({
   render: function () {
     var flightViewTemplate = $('#flightView-template').html();
     var flightViewHTML = _.template(flightViewTemplate);
-    // debugger;
+    debugger;
     this.$el.html(flightViewHTML(this.model.toJSON()));
+
+    var flight = this.model.toJSON();
+    var planeID = flight.airplane_id
+    var plane = app.burningAirplanes.get({id: planeID});
+    var rows = plane.attributes.rows
+    console.log(rows)
+    var columns = plane.attributes.columns
+    console.log(columns);
+
+    var seats = _.each(rows, function(row) {
+      _.each(row.columns, function(column){
+        console.log('hello');
+      });
+    });
 
     // // Save the actual flight object to a var
     // var flightDetails = this.model.toJSON();
