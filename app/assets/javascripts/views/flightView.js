0a09698d582ad7c1d138aa96fce43f3533c78767
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-15 13:10:56 +1100

    seating loops started

--- app/assets/javascripts/views/flightView.js
+++ app/assets/javascripts/views/flightView.js
@@ -17,13 +17,13 @@ app.FlightView = Backbone.View.extend({
 
     // // Airplane ID in a var
     // var airplane = flightDetails.airplane_id;
-  },
-  drawSeats: function () {
+
     var seatViewTemplate = $('#seatView-template').html();
     var seatViewHTML = _.template(seatViewTemplate);
 
     var flight = this.model.toJSON();
     var planeID = flight.airplane_id
+    // Object passed in to 'get' function makes sure we're finding the right one.
     var plane = app.burningAirplanes.get({id: planeID});
     var rows = plane.attributes.rows
     console.log(rows)
@@ -35,5 +35,33 @@ app.FlightView = Backbone.View.extend({
         console.log('hello');
       });
     });
+
+    var i, j;
+
+    for (i = 0; i < rows; i++) {
+      // make div for the row, give id of row-i
+      // might need to be i + 1
+      var row = $('<div class="row" id="row-' + (i + 1) + '"></div>');
+
+      for (j = 0; j < columns; j++) {
+        // make div for each column, give id of column-j
+        // append each to div with id=row-i
+        var column = $('<div class="column" id="column-' + (j + 1) + '"></div>');
+        var rowID = '#row-' + (i.toString());
+        console.log('i', i);
+        console.log('j', j);
+        console.log('row ID', rowID);
+        console.log(column);
+        $(rowID).append(column);
+      }
+
+      $('#main').append(row);
+    }
+
+
+    // give the rows and columns ids using the value of i so they are all unique
+    // all need to be appended to seat-grid
+    // columns need to be appended to relevant row
+
   }
 });
